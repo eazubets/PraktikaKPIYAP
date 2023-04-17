6c89831eeb23e02248921f3deb0dc463e36ab7d9
eazubets <119036394+eazubets@users.noreply.github.com>
2023-04-17 14:15:06 +0300

    Add files via upload

---
+++ PR1-4.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1_4._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод чисел N и K
+            Console.Write("Введите значение N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение K: ");
+            int K = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = 0; // Частное
+            int remainder = N; // Остаток
+
+            // Нахождение частного и остатка с использованием только сложения и вычитания
+            while (remainder >= K)
+            {
+                remainder -= K;
+                quotient++;
+            }
+
+            // Вывод результатов на экран
+            Console.WriteLine($"Частное от деления {N} на {K}: {quotient}");
+            Console.WriteLine($"Остаток от деления {N} на {K}: {remainder}");
+        }
+    }
+}
