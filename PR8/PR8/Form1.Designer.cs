6e46732dde3ec42982c5fde6c2d718ccdc0c984f
eazubets <119036394+eazubets@users.noreply.github.com>
2023-05-10 11:02:45 +0300

    5
    5

---
+++ PR8/PR8/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace PR8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddRectangle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddRectangle
+            // 
+            this.btnAddRectangle.Location = new System.Drawing.Point(110, 319);
+            this.btnAddRectangle.Name = "btnAddRectangle";
+            this.btnAddRectangle.Size = new System.Drawing.Size(104, 49);
+            this.btnAddRectangle.TabIndex = 0;
+            this.btnAddRectangle.Text = "button1";
+            this.btnAddRectangle.UseVisualStyleBackColor = true;
+            this.btnAddRectangle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 378);
+            this.Controls.Add(this.btnAddRectangle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddRectangle;
+    }
+}
+
---
+++ PR8/PR8/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR8
+{
+    public partial class Form1 : Form
+    {
+        private List<Drawable> shapes = new List<Drawable>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Drawable shape in shapes)
+            {
+                shape.Draw(e.Graphics);
+            }
+        }
+        private void AddShape(Drawable shape)
+        {
+            shapes.Add(shape);
+            Invalidate(); // Перерисовать форму
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle(50, 50, 100, 200);
+            AddShape(rect);
+        }
+    }
+    public interface Drawable
+    {
+        void Draw(Graphics g);
+    }
+
+    public abstract class Figure : Drawable
+    {
+        protected int x;
+        protected int y;
+
+        public Figure(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public abstract void Draw(Graphics g);
+    }
+
+    public class Rectangle : Figure
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int x, int y, int width, int height) : base(x, y)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.DrawRectangle(Pens.Black, x, y, width, height);
+        }
+    }
+    
+}
