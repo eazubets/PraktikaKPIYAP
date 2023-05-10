aeea82d59edc8b2303a93806530b82b3a82d4320
eazubets <119036394+eazubets@users.noreply.github.com>
2023-05-10 10:57:53 +0300

    7
    7

---
+++ PR7/PR7_1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PR7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Принять";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(439, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(439, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(439, 93);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(381, 179);
+            this.dataGridView1.TabIndex = 38;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(421, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Фамилия";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "предмет";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "дата";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 236);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ PR7/PR7_1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR7_1
+{
+    public partial class Form1 : Form
+    {
+        struct Teacher
+        {
+            public string surname;
+            public string exam;
+            public DateTime date;
+        }
+
+        Teacher[] teachersArray = new Teacher[0];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void UpdateTable()
+        {
+            // очищаем таблицу
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < teachersArray.Length; i++)
+            {
+                dataGridView1.Rows.Add(new object[] { teachersArray[i].surname, teachersArray[i].exam, teachersArray[i].date });
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text;
+            DateTime date = dateTimePicker1.Value;
+            for (int i = 0; i < teachersArray.Length; i++)
+            {
+                if (teachersArray[i].surname == surname && teachersArray[i].date == date)
+                {
+                    for (int j = i; j < teachersArray.Length - 1; j++)
+                    {
+                        teachersArray[j] = teachersArray[j + 1];
+                    }
+                    Array.Resize(ref teachersArray, teachersArray.Length - 1);
+                    UpdateTable();
+                    return;
+                }
+            }
+            MessageBox.Show("Запись не найдена");
+        }
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text;
+            DateTime date = dateTimePicker1.Value;
+            bool found = false;
+            for (int i = 0; i < teachersArray.Length; i++)
+            {
+                if (teachersArray[i].surname == surname && teachersArray[i].date == date)
+                {
+                    dataGridView1.CurrentCell = dataGridView1[0, i];
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                MessageBox.Show("Запись не найдена");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                // создаем новый экземпляр структуры Teacher и заполняем его данными
+                Teacher teacher = new Teacher();
+                teacher.surname = textBox1.Text;
+                teacher.exam = textBox2.Text;
+                teacher.date = dateTimePicker1.Value;
+
+                // увеличиваем размер массива и добавляем в него новую структуру
+                Array.Resize(ref teachersArray, teachersArray.Length + 1);
+                teachersArray[teachersArray.Length - 1] = teacher;
+
+                // очищаем текстовые поля и обновляем таблицу
+                textBox1.Text = "";
+                textBox2.Text = "";
+                dateTimePicker1.Value = DateTime.Today;
+                UpdateTable();
+        }
+    }
+}
